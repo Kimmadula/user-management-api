cae7d75b597776feda21cf4a7813f3016e8d34c7
Kimmadula <kimdwight.madula03@gmail.com>
2025-03-27 01:12:52 +0800

    Resolved

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,21 +1,10 @@
-/**
- * Gijan - User Listing and Retrieval
- * -------------------------------------
- * Route: GET /
- * Description: Lists all users.
- */
 router.get("/", async (_req, res) => {
   const userRepo = getRepository(User);
   const users = await userRepo.find(); // Gets all users
   res.json(users); // Returns user list
 });
 
-/**
- * Gijan - Get Single User
- * --------------------------
- * Route: GET /:id
- * Description: Gets a single user by ID.
- */
+
 router.get("/:id", async (req, res) => {
   const userRepo = getRepository(User);
   const user = await userRepo.findOneBy({ id: parseInt(req.params.id) });
